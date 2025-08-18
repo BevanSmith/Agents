7e14bfcbd19f4d27d36c1f98cffe9c3643ddca10
Bevan Smith <A0025105@wits.ac.za>
2025-08-18 10:27:12 +0200

    introduced Command for goto function

---
+++ 5_human_in_the_loop/2_command.py
@@ -0,0 +1,61 @@
+from langgraph.graph import START, END, StateGraph
+from langgraph.types import Command
+from typing import TypedDict
+import os
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+class State(TypedDict):
+    text: str
+
+
+def NodeA(state: State):
+    print("NodeA")
+
+    return Command(
+        goto="NodeB",
+        update={
+            "text": state["text"] + "a"
+        }
+    )
+
+def NodeB(state: State):
+    print("NodeB")
+
+    return Command(
+        goto="NodeC",
+        update={
+            "text":state["text"] + "b"
+        }
+    )
+
+def NodeC(state: State):
+    print("NodeC")
+
+    return Command(
+        goto=END,
+        update = {
+            "text": state["text"] + "c"
+        }
+    )
+
+graph = StateGraph(State)
+
+graph.add_node("NodeA", NodeA)
+graph.add_node("NodeB", NodeB)
+graph.add_node("NodeC", NodeC)
+
+
+graph.set_entry_point("NodeA")
+
+app=graph.compile()
+
+response= app.invoke({
+    "text": ""
+})
+
+response
